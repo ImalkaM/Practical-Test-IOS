4d4a9e52c93a124ecb378b77a279f730a5f33ad3
Imalka <Imalka.emedia@gmail.com>
2021-12-07 21:37:49 +0530

    implement network API class

---
+++ Practical Test IOS/Networking/ApiService.swift
@@ -0,0 +1,60 @@
+//
+//  ApiService.swift
+//  Practical Test IOS
+//
+//  Created by Imalka Muthukumara on 2021-12-07.
+//
+
+import Foundation
+
+
+class ApiService {
+    
+    private var dataTask: URLSessionDataTask?
+    
+    func getPopularMoviesData(completion: @escaping (Result<CandidateData, Error>) -> Void) {
+        
+        let popularMoviesURL = "https://randomuser.me/api/?results=50"
+        
+        guard let url = URL(string: popularMoviesURL) else {return}
+        
+        // Create URL Session - work on the background
+        dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            // Handle Error
+            if let error = error {
+                completion(.failure(error))
+                print("DataTask error: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                // Handle Empty Response
+                print("Empty Response")
+                return
+            }
+            print("Response status code: \(response.statusCode)")
+            
+            guard let data = data else {
+                // Handle Empty Data
+                print("Empty Data")
+                return
+            }
+            
+            do {
+                // Parse the data
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(CandidateData.self, from: data)
+                
+                // Back to the main thread
+                DispatchQueue.main.async {
+                    completion(.success(jsonData))
+                }
+            } catch let error {
+                completion(.failure(error))
+            }
+            
+        }
+        dataTask?.resume()
+    }
+}
